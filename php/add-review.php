09ff51abfc36e5898be87c8dcb9d2fda7759c97b
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2018-03-28 15:23:42 -0400

    created form for users to add reviews to page
    comments are submitted to DB

---
+++ php/add-review.php
@@ -0,0 +1,47 @@
+<?php
+
+// gets server information
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shopapparel";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$name = $_POST['name4'];
+$subject= $_POST['subject4'];
+$rating= $_POST['review'];
+$message= $_POST['message4'];
+$buyagain= $_POST['buyAgain'];
+$productname= $_POST['clothingname'];
+
+echo $name.'<br>';
+echo $subject.'<br>';
+echo $rating.'<br>';
+echo $message.'<br>';
+echo $buyagain.'<br>';
+echo $productname;
+
+
+$sql = "INSERT INTO `productreview`(`userName`, `subject`, `rating`, `message`, `buyAgain`, `clothingName`) VALUES ('$name','$subject','$rating','$message','$buyagain','$productname')";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
+
+?>
