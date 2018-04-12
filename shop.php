69e3d73f86c7c7767450455c5bb27919e9745ac8
HMinhLai <mdn6377@wmich.edu>
2018-04-12 18:50:43 -0400

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,4 +1,17 @@
 <?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: Login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: Login.php");
+  }
+?>
+<?php 
 	include("php/clothing-name.php");
 
 	// include("php/cart.php");
@@ -108,6 +121,25 @@
 	<!-- "transparent-header": makes the header transparent and pulls the banner to top -->
 	<!-- "gradient-background-header": applies gradient background to header -->
 	<!-- "page-loader-1 ... page-loader-6": add a page loader to the page (more info @components-page-loaders.html) -->
+	<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="shop.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
 	<body class="no-trans    ">
 
 		<!-- scrollToTop -->
