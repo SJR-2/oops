508108ed78765e89171fd00bbbc4d2bfcb5e9ca7
HMinhLai <mdn6377@wmich.edu>
2018-04-15 13:09:38 -0400

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,4 +1,16 @@
+<?php 
+  session_start(); 
 
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: Login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: Login.php");
+  }
+?>
 <?php 
 	include("php/clothing-name.php");
 	// include("php/cart.php");
@@ -94,7 +106,25 @@
 }
 </style>
 	</head>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="shop.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
 	<!-- body classes:  -->
 	<!-- "boxed": boxed layout mode e.g. <body class="boxed"> -->
 	<!-- "pattern-1 ... pattern-9": background patterns for boxed layout mode e.g. <body class="boxed pattern-1"> -->
